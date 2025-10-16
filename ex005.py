b08867dfd2de0a213b26e785eaed0bf4e91104fa
HabacuqueCL <habacuque.cavalcante@gmail.com>
2025-10-16 19:44:50 -0300

    continuação aula07

--- ex005.py
+++ ex005.py
@@ -26,4 +26,32 @@
 #print('O valor informado foi: {}\n Em centímetros: {}\n Em milímetros: {}'.format(valor, cm, mm))
 
 #Faça um programa de leia um número inteiro qualquer e mostre na tela a sua tabuada.
-valor = int(input('Insira um valor: '))
+#valor = int(input('Insira um valor: '))
+#mult1 = valor * 1
+#mult2 = valor * 2
+#mult3 = valor * 3
+#mult4 = valor * 4
+#mult5 = valor * 5
+#mult6 = valor * 6
+#mult7 = valor * 7
+#mult8 = valor * 8
+#mult9 = valor * 9
+
+#print('A tabuada do {} é: \n {}x1={}  {}x2={}  {}x3={} \n {}x4={} {}x5={}  {}x6={} \n {}x7={} {}x8={}  {}x9={}'.format(valor,valor,mult1,valor,mult2,valor,mult3,valor,mult4,valor,mult5,valor,mult6,valor,mult7,valor,mult8,valor,mult9))
+
+#Crie um programa que leia quanto dinheiro uma pessoa na carteira e mostre quantos dolares ela pode comprar.
+#valorCarteira = float(215)
+#valorDolar = float(3.27)
+#print('Dolar hoje: US${}'.format(valorDolar))
+#print('Sua carteira: BR${}'.format(valorCarteira))
+#maxCompra = valorCarteira // valorDolar
+#print('Valor máximo de compra: US${}'.format(maxCompra))
+
+#Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la, sabendo que cada litro de tinta, pinta uma área de 2m².
+print('Informe a largura da parede em metros.')
+valorLargura = float(input('Largura: '))
+valorAltura = float(input('Altura: '))
+valorArea = valorAltura * valorLargura
+print('Área da sua parede: {}m²'.format(valorArea))
+
+
