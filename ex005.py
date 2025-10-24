b694511e878d5aebc7588a06cd0394004042ab7f
HabacuqueCL <habacuque.cavalcante@gmail.com>
2025-10-23 22:55:50 -0300

    resolução exercicios 11,12 e 13

--- ex005.py
+++ ex005.py
@@ -1,10 +1,10 @@
-#Crie um programa que leia um número inteiro e mostre na tela o seu sucessor seu antecessor.
+#05 Crie um programa que leia um número inteiro e mostre na tela o seu sucessor seu antecessor.
 #valor = int(input('Digite um número: '))
 #sucessor = valor - 1
 #antecessor = valor + 1
 #print('Valor informado: {}\n Sucessor: {}\n Antecessor: {}'.format(valor, sucessor, antecessor))
 
-#Crie um algoritmo que leia um algoritmo e mostre o seu dobro, triplo e raiz quadrada.
+#06 Crie um algoritmo que leia um algoritmo e mostre o seu dobro, triplo e raiz quadrada.
 #valor = int(input('Informe um número: '))
 #dobro = valor*2
 #triplo = valor*3
@@ -13,19 +13,19 @@
 #print('O trplo de {} é: {}'.format(valor, triplo))
 #print('A raiz quadrada de {} é: {}'.format(valor, raizQuadrada))
 
-#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+#07 Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 #nota1 = 7
 #nota2 = 4
 #media = (7+4)/2
 #print(media)
 
-#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+#08 Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 #valor = float(input('Informe o valor em metros: '))
 #cm = valor * 100
 #mm = valor * 1000
 #print('O valor informado foi: {}\n Em centímetros: {}\n Em milímetros: {}'.format(valor, cm, mm))
 
-#Faça um programa de leia um número inteiro qualquer e mostre na tela a sua tabuada.
+#09 Faça um programa de leia um número inteiro qualquer e mostre na tela a sua tabuada.
 #valor = int(input('Insira um valor: '))
 #mult1 = valor * 1
 #mult2 = valor * 2
@@ -39,7 +39,7 @@
 
 #print('A tabuada do {} é: \n {}x1={}  {}x2={}  {}x3={} \n {}x4={} {}x5={}  {}x6={} \n {}x7={} {}x8={}  {}x9={}'.format(valor,valor,mult1,valor,mult2,valor,mult3,valor,mult4,valor,mult5,valor,mult6,valor,mult7,valor,mult8,valor,mult9))
 
-#Crie um programa que leia quanto dinheiro uma pessoa na carteira e mostre quantos dolares ela pode comprar.
+#10 Crie um programa que leia quanto dinheiro uma pessoa na carteira e mostre quantos dolares ela pode comprar.
 #valorCarteira = float(215)
 #valorDolar = float(3.27)
 #print('Dolar hoje: US${}'.format(valorDolar))
@@ -47,11 +47,27 @@
 #maxCompra = valorCarteira // valorDolar
 #print('Valor máximo de compra: US${}'.format(maxCompra))
 
-#Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la, sabendo que cada litro de tinta, pinta uma área de 2m².
-print('Informe a largura da parede em metros.')
-valorLargura = float(input('Largura: '))
-valorAltura = float(input('Altura: '))
-valorArea = valorAltura * valorLargura
-print('Área da sua parede: {}m²'.format(valorArea))
+#11 Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la, sabendo que cada litro de tinta, pinta uma área de 2m².
+#print('Informe a altura e a largura da parede em metros.')
+#valorAltura = float(input('Altura: '))
+#valorLargura = float(input('Largura: '))
+#valorArea = valorAltura * valorLargura
+#rendTinta = 2.0
+#tintaNecessária = (valorArea/rendTinta/1)*1 #Levando em conta 1 demão de tinta
+#print('Área da sua parede: {}m²'.format(valorArea))
+#print('A quantidade de tinra necessaria para pintar {}m² é: {} litros '.format(valorArea, tintaNecessária))
 
+#12 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+#valorProduto = float(input('Informe o valor do produto: '))
+#informarDesc = float(input('Informe o desconto a ser aplicado: '))
+#aplicarDesc = (informarDesc/100)*valorProduto
+#print('Valor inicial do produto: R${}\n Valor de desconto aplicado: {}%\n Valor final do #produto: R${}'.format(valorProduto, informarDesc,aplicarDesc))
+
+#13 Faça um algoritmo que leia o salário de um funcionário e mostre o seu novo salário, com 15% de aumento.
+
+valSalario = int(input('Informe salário: '))
+valAdicional = int(input('Informe % à adicionar: '))
+calcAumento = (valSalario/100)*valAdicional+valSalario
+print('Salário informado: R${}\n Porcentagem de aumento: {}\n Salário final: {}'.format(valSalario,valAdicional,calcAumento))
 
