213a70de9b3dec42681fd5fca6d4998afcabeef9
HabacuqueCL <habacuque.cavalcante@gmail.com>
2025-11-04 23:00:25 -0300

    correção ex. 07,08,09 e 10

--- ex005.py
+++ ex005.py
@@ -14,16 +14,20 @@
 #print('A raiz quadrada de {} é: {:.2f}'.format(valor, raizQuadrada))
 
 #07 Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-nota1 = input('Digite a primeira nota: ')
-nota2 = input('Digite a segunda nota: ')
-media = (7+4)/2
-print('A média das notas {} e {} é: {}'.format(nota1,nota2,media))
+#nota1 = float(input('Digite a primeira nota: '))
+#nota2 = float(input('Digite a segunda nota: '))
+#media = (nota1+nota2)/2
+#print('A média das notas {} e {} é: {:.5f}'.format(nota1,nota2,media))
 
 #08 Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 #valor = float(input('Informe o valor em metros: '))
+#km = valor / 1000
+#hm = valor / 100
+#dam = valor / 10
+#dm = valor * 10
 #cm = valor * 100
 #mm = valor * 1000
-#print('O valor informado foi: {}\n Em centímetros: {}\n Em milímetros: {}'.format(valor, cm, mm))
+#print('Valor em metros: {}m \nQuilômetros: {}km  \nHectômetros: {}hm \nDecâmetro: {}dam \nDecímetro: {:.0f}dm \nCentímetros: {:.0f}cm \nMilímetros: {:.0f}mm '.format(valor, km, hm, dam, dm, cm, mm))
 
 #09 Faça um programa de leia um número inteiro qualquer e mostre na tela a sua tabuada.
 #valor = int(input('Insira um valor: '))
@@ -37,15 +41,18 @@ print('A média das notas {} e {} é: {}'.format(nota1,nota2,media))
 #mult8 = valor * 8
 #mult9 = valor * 9
 
-#print('A tabuada do {} é: \n {}x1={}  {}x2={}  {}x3={} \n {}x4={} {}x5={}  {}x6={} \n {}x7={} {}x8={}  {}x9={}'.format(valor,valor,mult1,valor,mult2,valor,mult3,valor,mult4,valor,mult5,valor,mult6,valor,mult7,valor,mult8,valor,mult9))
+#print('#'+('-'*21)+'#')
+#print('| A tabuada do {} é: \n| {}x1 = {}  \n| {}x2 = {}  \n| {}x3 = {} \n| {}x4 = {} \n| {}x5 = {} \n| {}x6 = {} \n| {}x7 = {} \n| {}x8 = {}  \n| {}x9 = {}'.format(valor,valor,mult1,valor,mult2,valor,mult3,valor,mult4,valor,mult5,valor,mult6,valor,mult7,valor,mult8,valor,mult9))
+#print('#'+('-'*21)+'#')
 
-#10 Crie um programa que leia quanto dinheiro uma pessoa na carteira e mostre quantos dolares ela pode comprar.
-#valorCarteira = float(215)
-#valorDolar = float(3.27)
-#print('Dolar hoje: US${}'.format(valorDolar))
-#print('Sua carteira: BR${}'.format(valorCarteira))
-#maxCompra = valorCarteira // valorDolar
-#print('Valor máximo de compra: US${}'.format(maxCompra))
+#10 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
+#Considere US$:1.00 = BR$3.27
+valorCarteira = float(input('Quanto você tem na carteira: BR$'))
+valorDolar = float(3.27)
+print('Dolar hoje: US${:.2f}'.format(valorDolar))
+print('Sua carteira: BR${:.2f}'.format(valorCarteira))
+maxCompra = valorCarteira / valorDolar
+print('Valor máximo de compra: US${:.2f}'.format(maxCompra))
 
 #11 Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la, sabendo que cada litro de tinta, pinta uma área de 2m².
 #print('Informe a altura e a largura da parede em metros.')
