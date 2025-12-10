d8a93e62dde8f7e5263370a0cf3f3866c1c9d2cd
HabacuqueCL <habacuque.cavalcante@gmail.com>
2025-12-10 13:37:21 -0300

    resolução exercicios 11 e 12

--- ex005.py
+++ ex005.py
@@ -47,12 +47,20 @@
 
 #10 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
 #Considere US$:1.00 = BR$3.27
-valorCarteira = float(input('Quanto você tem na carteira: BR$'))
-valorDolar = float(3.27)
-print('Dolar hoje: US${:.2f}'.format(valorDolar))
-print('Sua carteira: BR${:.2f}'.format(valorCarteira))
-maxCompra = valorCarteira / valorDolar
-print('Valor máximo de compra: US${:.2f}'.format(maxCompra))
+#valorCarteira = float(input('Quanto você tem na carteira: BR$'))
+#valorDolar = float(5.467)
+#valorEuro = float(6.360)
+#valorIene = float(0.03492)
+#print('Dolar hoje: USD {:.2f}'.format(valorDolar))
+#print('Euro hoje: EUR {:.2f}'.format(valorEuro))
+#print('Iene hoje: JPY {:.2f}'.format(valorIene))
+#print('Sua carteira: BRL{:.2f}'.format(valorCarteira))
+#maxCompraDolar = valorCarteira / valorDolar
+#maxCompraEuro = valorCarteira / valorEuro
+#maxCompraIene = valorCarteira / valorIene
+#print('Valor máximo de compra (Dolar): USD {:.2f}'.format(maxCompraDolar))
+#print('Valor máximo de compra (Euro): EUR {:.2f}'.format(maxCompraEuro))
+#print('Valor máximo de compra (Iene): JPY {:.2f}'.format(maxCompraIene))
 
 #11 Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la, sabendo que cada litro de tinta, pinta uma área de 2m².
 #print('Informe a altura e a largura da parede em metros.')
@@ -62,14 +70,14 @@ print('Valor máximo de compra: US${:.2f}'.format(maxCompra))
 #rendTinta = 2.0
 #tintaNecessária = (valorArea/rendTinta/1)*1 #Levando em conta 1 demão de tinta
 #print('Área da sua parede: {}m²'.format(valorArea))
-#print('A quantidade de tinra necessaria para pintar {}m² é: {} litros '.format(valorArea, tintaNecessária))
+#print('A quantidade de tinta necessária para pintar {}m² é: {} litros '.format(valorArea, tintaNecessária))
 
 #12 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-#valorProduto = float(input('Informe o valor do produto: '))
-#informarDesc = float(input('Informe o desconto a ser aplicado: '))
-#aplicarDesc = (informarDesc/100)*valorProduto
-#print('Valor inicial do produto: R${}\n Valor de desconto aplicado: {}%\n Valor final do #produto: R${}'.format(valorProduto, informarDesc,aplicarDesc))
+valorProduto = float(input('Informe o valor do produto: '))
+informarDesc = float(input('Informe o desconto a ser aplicado: '))
+calculoDesc = valorProduto-(valorProduto*informarDesc/100)
+print('Valor inicial do produto: R${}\n Valor de desconto aplicado: {}%\n Valor final do produto: R${:.2f}'.format(valorProduto, informarDesc,calculoDesc))
 
 #13 Faça um algoritmo que leia o salário de um funcionário e mostre o seu novo salário, com 15% de aumento.
 
