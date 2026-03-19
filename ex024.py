90cbb67b494a75c9635e0279e43b6154914f2ad9
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-03-19 17:25:43 -0300

    ex024 realizado

--- ex024.py
+++ ex024.py
@@ -1,5 +1,8 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com "SANTO".
 
 nomeCidade = input('Digite o nome da sua cidade: ')
-splitCidade = nomeCidade.split()
-print('SANTO' in splitCidade[0])
+palavra = 'SANTO'
+if palavra in nomeCidade:
+    print('O nome da cidade começa com SANTO')
+else:
+    print('O nome da cidade não começa com SANTO')
