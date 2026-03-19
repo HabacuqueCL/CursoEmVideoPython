5146ed7b54a529ad47a89d24f1f504ce68cb9ffc
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-03-19 19:44:22 -0300

    resolucao ex022

--- ex022.py
+++ ex022.py
@@ -6,8 +6,16 @@
 
 nome = input('Digite o seu nome completo: ')
 retirarEspacos = nome.replace(' ','')
-dividir = nome.split()
+dividirNome = nome.split()
 print('Seu nome em maiusculo: {}'.format(nome.upper()))
 print('Seu nome em minusculo: {}'.format(nome.lower()))
 print('Seu nome possui {} letras no total.'.format(len(retirarEspacos)))
-print('Seu primeiro nome tem {} letras.'.format((len(dividir[0]))))
+print('Seu primeiro nome tem {} letras.'.format((len(dividirNome[0]))))
+
+
+#Resolição do Guanabara
+#nome = str(input('Digite o seu nome completo: ')).strip() #Strip elminia os espacos no começo/fim da string
+#print('Seu nome em maiusculo: {}'.format(nome.upper()))
+#print('Seu nome em minusculo: {}'.format(nome.lower()))
+#print('Seu nome possui {} letras no total.'.format(len(nome)-nome.count(' '))) #Vai contar quantos caracteres tem ao total com espaços com o len e depois subtrair os espaços com count
+#print('Seu primeiro nome tem {} letras.'.format(nome.find(' '))) #Vai indicar a posição do primeiro espaço sendo assim, como começa a contagem do 0 a posição que sair é a quantidade de letras da primeira palavra.
