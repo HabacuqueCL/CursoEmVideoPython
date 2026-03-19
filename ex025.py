1d9c5d43473516b9b14d2e5074253e5d4b956173
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-03-19 17:39:06 -0300

    resolução ex025

--- ex025.py
+++ ex025.py
@@ -2,3 +2,7 @@
 
 informarNome = input('Digite o nome de uma pessoa: ')
 verificarPalavra = 'SANTO'
+if verificarPalavra in informarNome:
+    print('Você tem a palavra SILVA no seu nome.')
+else:
+    print('Você não tem a palavra SILVA no seu nome.')    
