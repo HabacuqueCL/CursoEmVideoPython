8e1bb6ff6308eb202ebbecff754949a7c92ee9c8
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-03-19 18:42:45 -0300

    ex026 feito

---
+++ ex026.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia uma frase pelo teclado e mostre:
+#Quantas vezes aparece a letra "A";
+#Em que posição ela aparece a primeira vez;
+#Em que posição ela aparece a ultima vez.
+
+frase = input('Digite uma frase: ')
+print('A letra "A" aparece {}x na frase.'.format(frase.count('A')))
+print('A letra "A" aparece a primeira vez na posição {} dentro da frase.'.format(frase.find('A')))
+print('A letra "A" aparece a última vez na posição {} dentro da frase'.format(frase.rfind('A')))
