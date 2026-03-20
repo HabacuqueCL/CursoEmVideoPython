7ae8e416c14b728a289efeb14973d88d1c8de073
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-03-19 23:38:28 -0300

    resolucao ex027

--- ex027.py
+++ ex027.py
@@ -1,6 +1,14 @@
 #Faca um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente
 
-nome = input('Insira o seu nome: ')
+nome = input('Insira o seu nome: ').strip()
 splitNome = nome.split()
 print('Primeiro nome: {}'.format(splitNome[0]))
 print('Último nome: {}'.format(splitNome[-1]))
+
+#Resolucao guanabara
+
+#n = str(input('Digite seu nome completo: ')).strip()
+#nome = n.split()
+#print('Muito prazer em te conhecer!')
+#print('Seu primeiro nome é: {}'.format(nome[0]))
+#print('Seu último nome é: {}'.format(nome[len(nome)-1]))
