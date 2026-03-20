af9223f1927c9710f913bbd7aba17791be791756
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-03-19 23:27:28 -0300

    resolucao ex026

--- ex026.py
+++ ex026.py
@@ -3,7 +3,7 @@
 #Em que posição ela aparece a primeira vez;
 #Em que posição ela aparece a ultima vez.
 
-frase = input('Digite uma frase: ')
+frase = input('Digite uma frase: ').strip().upper()
 print('A letra "A" aparece {}x na frase.'.format(frase.count('A')))
-print('A letra "A" aparece a primeira vez na posição {} dentro da frase.'.format(frase.find('A')))
-print('A letra "A" aparece a última vez na posição {} dentro da frase'.format(frase.rfind('A')))
+print('A letra "A" aparece a primeira vez na posição {} dentro da frase.'.format(frase.find('A')+1))
+print('A letra "A" aparece a última vez na posição {} dentro da frase'.format(frase.rfind('A')+1))
