8a1a86c98649076d5f3bb2577fded44651c34a7c
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-04-07 18:50:31 -0300

    resolução exe029

---
+++ ex029.py
@@ -0,0 +1,8 @@
+#Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+velCarro = int(input('Informe a velocidade do carro: '))
+if velCarro>=81:
+    print('Seu carro ultrapasou {}Km/h acima da velocidade permitida e será multado em R${:.2f}.'.format((velCarro-80), (velCarro-80)*7))
+else:
+    print('Seu carro está dentro do limite de velocidade permitido.')
+print("Você não está no Velozes e Furiosos, respeite a velocidade permitida.")
