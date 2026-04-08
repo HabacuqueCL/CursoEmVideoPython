89308f122d254aad2843c6d573aa320a226efcc5
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-04-08 15:59:22 -0300

    ...

---
+++ ex033.py
@@ -0,0 +1,5 @@
+#Faca um programa que leia três números e mostre qual é o MAIOR e qual é o MENOR. 
+
+num1 = int(input('Informe o primeiro número: '))
+num2 = int(input('Informe o segundo número: '))
+num3 = int(input('Informe o terceiro número: '))
