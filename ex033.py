54625410da5e5559cfeb1249cc34dfccb31b5dd5
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-04-09 11:59:30 -0300

    resolução ex033

--- ex033.py
+++ ex033.py
@@ -3,3 +3,17 @@
 num1 = int(input('Informe o primeiro número: '))
 num2 = int(input('Informe o segundo número: '))
 num3 = int(input('Informe o terceiro número: '))
+
+maior = num1
+if num2 > maior:
+    maior = num2
+if num3 > maior:
+    maior = num3
+
+menor = num1
+if num2 < menor:
+    menor = num2
+if num3 < menor:
+    menor = num3
+
+print('Numeros inseridos {}, {}, {}. O maior é {} e o menor é {}.'.format(num1, num2, num3, maior, menor))
