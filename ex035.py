e28a2fe5930054a52a70defb8ff0726e8fbcc40b
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-04-09 19:21:57 -0300

    resolução ex035

--- ex035.py
+++ ex035.py
@@ -1,3 +1,9 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem formar um triângulo ou não.
 
-reta1 = 
+n1 = input('Valor 1: ')
+n2 = input('Valor 2: ')
+n3 = input('Valor 3: ')
+if n1+n2>n3 and n1+n3>n2 and n2+n3>n1:
+    print('É possível formar um triângulo com as retas informadas.')
+else:
+    print('Não é possível formar um triângulo com as retas informadas.')
