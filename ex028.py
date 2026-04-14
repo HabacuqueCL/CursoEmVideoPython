3a061678fe39bf035847033fec1fbd12828b50d5
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-04-14 17:20:48 -0300

    correcao ex029

--- ex028.py
+++ ex028.py
@@ -1,8 +1,8 @@
 #Escreva um programa que paça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo comutador. O programa deverá escrever na tela de o usuário venceu ou perdeu.
 
-import random
+from random import randint
 print('O sistema está escolhendo um número...')
-num = random.randint(1,5)
+num = randint(0,5)
 escolheNum = int(input('Adivinhe o número que o sistema escolheu: '))
 if num == escolheNum:
     print('Número gerado pelo sistema: {}'.format(num))
--- ex029.py
+++ ex029.py
@@ -1,7 +1,7 @@
 #Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
 
 velCarro = int(input('Informe a velocidade do carro: '))
-if velCarro>=81:
+if velCarro>80:
     print('Seu carro ultrapasou {}Km/h acima da velocidade permitida e será multado em R${:.2f}.'.format((velCarro-80), (velCarro-80)*7))
 else:
     print('Seu carro está dentro do limite de velocidade permitido.')
