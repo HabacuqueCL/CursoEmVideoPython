d44d3b0bd447b59f97ad7a125381664545d64207
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-04-16 12:57:17 -0300

    ...

--- ex035.py
+++ ex035.py
@@ -11,12 +11,12 @@ else:
 
 
 #Resolução Guanabara
-r1 = float(input('Primeiro segmento: '))
-r2 = float(input('Segundo segmento: '))
-r3 = float(input('Terceiro segmento: '))
-if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os segmentos PODEM FORMAR UM TRIANGULO.')
-else:
-    print('Os segmentos NÃO PODEM FORMAR UM TRIANGULO.')
+#r1 = float(input('Primeiro segmento: '))
+#r2 = float(input('Segundo segmento: '))
+#r3 = float(input('Terceiro segmento: '))
+#if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+#    print('Os segmentos PODEM FORMAR UM TRIANGULO.')
+#else:
+#    print('Os segmentos NÃO PODEM FORMAR UM TRIANGULO.')
 
 #Cálculo: O 1º segmento tem que ser menor que os outros dois segmentos. o 2º segmento tem que ser menor que os outros dois e o 3º segmento tem que ser menor que os outros dois segmentos. Todos eles sendo true, formam um triangulo, do contrário, não formam.
