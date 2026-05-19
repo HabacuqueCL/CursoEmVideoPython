1497ce0f175eaf41b2f78561120a3043ace33c25
HabacuqueCL <habacuque.cavalcante@gmail.com>
2026-05-19 20:42:51 -0300

    criacao do ex 36

---
+++ ex036.py
@@ -0,0 +1,2 @@
+#Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o VALOR DA CASA, o SALARIO do comprpador e em QUANTOS ANOS ele vai pagar. Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
